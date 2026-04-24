56b9cc5fe5d049016b64eb7060b5282fbbec9cb5
jerrypm <jerrypurnamamaulid@gmail.com>
2026-04-24 14:37:32 +0700

    Initial Commit

---
+++ tiktok-remove-watermark/tiktok_remove_watermarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktok_remove_watermarkApp.swift
+//  tiktok-remove-watermark
+//
+//  Created by Jeri Purnama Maulid on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct tiktok_remove_watermarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
